7c3acadc8e1f33b025f1d952fc609d6dca18f7c5
Chribtoper <xcstolfax@gmail.com>
2019-06-21 14:19:55 -0400

    Added some data sanitization to my loop

--- src/Simulation.java
+++ src/Simulation.java
@@ -7,17 +7,20 @@ public class Simulation {
 
     // public ArrayList<Item>
     public void loadItems() {
+
+
+
         try {
             int num = 1;
             File file = new File("phase-" +num+ ".txt");
             Scanner scanner = new Scanner(file);
-
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
-
+                String line[] = scanner.nextLine().split("=");
+                Item item = new Item(line[0], Integer.parseInt(line[1]));
             }
-        } catch(FileNotFoundException exception) {
-            System.out.println(exception);
+        } catch (FileNotFoundException f) {
+            System.out.println(f);
+            System.out.println("File not found.");
         }
     }
 }
